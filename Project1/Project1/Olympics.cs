55e45b53b955b3f9739bbc32c3dac496dfbe8cc8
Max Dupler <maxdupler@Maxs-MBP.lan>
2023-09-20 20:39:44 -0400

    adding project1

---
+++ Project1/Project1/Olympics.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1;
+public class Olympics {
+	public static List<Participant>? participants;
+
+	public static List<Participant> ReadParticipants(string fname) {
+		participants = new List<Participant>();
+		using (StreamReader input = new StreamReader(fname)) {
+			string? header = input.ReadLine();  // Read and discard the header
+			while (!input.EndOfStream) {
+				string? line = input.ReadLine();
+				if (line == null) {
+					continue;
+				}
+				Participant p = new Participant(line);
+				participants.Add(p);
+			}
+		}
+		return participants;
+	}
+	public static void Main(string[] args) {
+		participants = ReadParticipants("olympics.tsv");
+
+		while (true) {
+			Console.Write(">> ");
+			string? userInput = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(userInput))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid command.");
+                continue;
+            }
+
+            string[] inputTok = userInput.Split(' ');
+            string command = inputTok[0];
+            List<string> arguments = new List<string>();
+            if (inputTok.Length > 1)
+            {
+                arguments = parseArguments(inputTok);
+            }
+
+
+            switch (command)
+            {
+                case "hosts":
+                    listHosts();
+                    break;
+                case "count":
+                    if (arguments.Count == 2 && int.TryParse(arguments[0], out int year))
+                    {
+                        string country = arguments[1];
+                        CountMedalsByYearAndCountry(year, country);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid format. Usage: count year country");
+                    }
+                    break;
+                case "golds":
+                    if (arguments.Count == 2 && int.TryParse(arguments[0], out int goldYear))
+                    {
+                        string goldCountry = arguments[1];
+                        ListGoldMedalWinners(goldYear, goldCountry);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid format. Usage: golds year country");
+                    }
+                    break;
+                case "podium":
+                    if (arguments.Count == 3 && int.TryParse(arguments[0], out int podiumYear))
+                    {
+                        string podiumSeason = arguments[1];
+                        string podiumEvent = arguments[2];
+                        ListMedalWinnersForEvent(podiumYear, ParseSeason(podiumSeason), podiumEvent);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid format. Usage: podium year season event");
+                    }
+                    break;
+                case "exit":
+                    ExitProgram();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid command. Please try again");
+                    break;
+            }
+		}
+	}
+
+    private static Participant.SeasonType ParseSeason(string season)
+    {
+        return season.Equals("Summer")
+            ? Participant.SeasonType.Summer
+            : Participant.SeasonType.Winter;
+    }
+
+    private static void listHosts()
+	{
+        List<string> hosts = new List<string>();
+        foreach (Participant p in participants)
+        {
+            if (!hosts.Contains(p.Location))
+            {
+                hosts.Add(p.Location);
+            }
+        }
+
+        hosts.Sort(); // Sort the list of host cities
+        foreach (string host in hosts)
+        {
+            Console.WriteLine(host);
+        }
+    }
+
+    private static void CountMedalsByYearAndCountry(int year, string country)
+    {
+        int count = participants.Count(p => p.Year == year && string.Equals(p.Country, country) && p.Medal != Participant.MedalType.None);
+        Console.WriteLine($"Total medals won by {country} in {year}: {count}");
+    }
+
+    private static void ListGoldMedalWinners(int year, string country)
+    {
+        var goldMedalWinners = participants
+            .Where(p => p.Year == year && string.Equals(p.Country, country) && p.Medal == Participant.MedalType.Gold)
+            .Select(p => p.Name)
+            .Distinct();
+
+        foreach (string winner in goldMedalWinners)
+        {
+            Console.WriteLine(winner);
+        }
+    }
+
+
+    private static void ListMedalWinnersForEvent(int year, Participant.SeasonType season, string eventName)
+    {
+        var medalWinners = participants
+            .Where(p => p.Year == year && p.Season == season && string.Equals(p.Event, eventName) && p.Medal != Participant.MedalType.None)
+            .OrderBy(p => p.Medal) // Order by medal type (Gold, Silver, Bronze)
+            .ThenBy(p => p.Name)   // Then order by athlete name
+            .GroupBy(p => p.Medal)
+            .ToList();
+
+        foreach (var medalGroup in medalWinners)
+        {
+            foreach (var participant in medalGroup)
+            {
+                Console.WriteLine($"{participant.Medal} - {participant.Name}");
+            }
+        }
+    }
+
+
+    private static void ExitProgram()
+    {
+        Environment.Exit(0);
+    }
+
+    public static List<string> parseArguments(string[] inputTok)
+    {
+        List<string> arguments = new List<string>();
+        arguments.Add(inputTok[1]);
+        if (inputTok[2] == "Summer" || inputTok[2] == "Winter")
+        {
+            arguments.Add(inputTok[2]);
+            arguments.Add(string.Join(" ", inputTok.Skip(3)));
+        } else
+        {
+            arguments.Add(string.Join(" ", inputTok.Skip(2)));
+        }
+
+        return arguments;
+    }
+}
---
+++ Project1/Project1/Participant.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1;
+public class Participant {
+	public enum MedalType { None, Bronze, Silver, Gold };
+	public enum SeasonType { Summer, Winter };
+	public enum GenderType { Female, Male };
+
+
+	public int ID { get; set; }
+	public String Name { get; set; }
+	public GenderType Gender { get; set; }
+	public int Age { get; set; }
+	public String Country { get; set; }
+	public int Year { get; set; }
+	public SeasonType Season { get; set; }
+	public String Location { get; set; }
+	public String Sport { get; set; }
+	public String Event { get; set; }
+	public MedalType Medal { get; set; }
+
+	public Participant(String str) {
+		string[] toks = str.Split(new char[] { '\t' });
+
+        ID = int.Parse(toks[0]);
+        Name = toks[1];
+        Gender = (toks[2] == "M") ? GenderType.Male : GenderType.Female;
+        if (int.TryParse(toks[3], out int ageValue))
+        {
+            Age = ageValue;
+        }
+        else
+        {
+            Age = -1;
+        }
+        Country = toks[6];
+        Year = int.Parse(toks[9]);
+        Season = (toks[10] == "Summer") ? SeasonType.Summer : SeasonType.Winter;
+        Location = toks[11];
+        Sport = toks[12];
+        Event = toks[13];
+
+        string medalValue = toks[14];
+        if (medalValue == "Bronze")
+        {
+            Medal = MedalType.Bronze;
+        }
+        else if (medalValue == "Silver")
+        {
+            Medal = MedalType.Silver;
+        }
+        else if (medalValue == "Gold")
+        {
+            Medal = MedalType.Gold;
+        }
+        else
+        {
+            Medal = MedalType.None;
+        }
+    }
+}
